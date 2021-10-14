5238fdc1f6f803b62303ba9828c37e005e45e49a
Kawal Anand <83752105+kawalanand@users.noreply.github.com>
2021-10-14 23:09:02 +0530

    Add files via upload

---
+++ assignment_9/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Todo from './components/todo';
+<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+function App() {
+  return (
+    <div>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
